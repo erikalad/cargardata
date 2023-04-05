f215607386fdb08a2b3ff73af2ace0bf8bcb7040
Erika Ladner <erikaladner5@gmail.com>
2023-04-05 13:59:53 -0300

    form

--- src/components/descargar.jsx
+++ src/components/descargar.jsx
@@ -1,5 +1,10 @@
 import * as XLSX from 'xlsx';
 import React from 'react';
+import { Button, Form, Input, InputNumber, Select } from 'antd';
+import FormItem from 'antd/es/form/FormItem';
+import TextArea from 'antd/es/input/TextArea';
+import './descargar.css'
+
 
 function generateExcel(formData) {
   const worksheet = XLSX.utils.json_to_sheet([formData]);
@@ -9,57 +14,62 @@ function generateExcel(formData) {
   return fileBuffer;
 }
 
-
-/*  function downloadExcel(formData) {
-    const fileBuffer = generateExcel(formData);
-    const blob = new Blob([fileBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.setAttribute('download', 'form-data.xlsx');
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }  */
-
   export default function Descargar(){
 
-    function downloadExcel() {
-        const formData = new FormData(document.querySelector('form'));
-        const data = {
-          name: formData.get('name'),
-          email: formData.get('email'),
-          age: formData.get('age'),
-          phone: formData.get('phone')
-        };
-        const fileBuffer = generateExcel(data);
-        // Resto del código para descargar el archivo Excel
-        const blob = new Blob([fileBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-        const url = URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'form-data.xlsx');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }
+    function downloadExcel(values) {
+      console.log(values)
+      const data = {
+        Dashboard: values.dashboard,
+        Series: values.series,
+        SubSeries: values.Subseries,
+        Texto: values.texto,
+      };
+      const fileBuffer = generateExcel(data);
+      const blob = new Blob([fileBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'form-data.xlsx');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+
+
     return(
-       
-        <form>
-        <label htmlFor="name">Nombre:</label>
-        <input type="text" id="name" name="name" />
-      
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-      
-        <label htmlFor="age">Edad:</label>
-        <input type="number" id="age" name="age" />
-                  
-        <label htmlFor="phone">Teléfono:</label>
-        <input type="text" id="phone" name="phone" />
-      
-        <button type="button" onClick={downloadExcel}>Descargar Excel</button>
-      </form>
-     
+        <div className='form'>
+      <div className='title'>Cargar datos</div>
+      <Form onFinish={downloadExcel}>
+      <FormItem htmlFor="dashboard" label="Dashboard" name="dashboard">
+      <Input type="text" id="dashboard"  />
+      </FormItem> 
+
+      <Form.Item label="Series" name="series">
+      <Select>
+        <Select.Option value="Demo1">Demo1</Select.Option>
+        <Select.Option value="Demo2">Demo2</Select.Option>
+        <Select.Option value="Demo3">Demo3</Select.Option>
+        <Select.Option value="Demo4">Demo4</Select.Option>
+        <Select.Option value="Demo5">Demo5</Select.Option>
+      </Select>
+    </Form.Item>
+
+    <Form.Item label="Subseries" name="Subseries">
+      <Select>
+        <Select.Option value="Demo1">Demo1</Select.Option>
+        <Select.Option value="Demo2">Demo2</Select.Option>
+        <Select.Option value="Demo3">Demo3</Select.Option>
+        <Select.Option value="Demo4">Demo4</Select.Option>
+        <Select.Option value="Demo5">Demo5</Select.Option>
+      </Select>
+    </Form.Item>
+
+      <FormItem htmlFor="texto" label="Texto:" name="texto">
+      <TextArea type="text" id="texto"  />
+      </FormItem>
+
+      <Button type="primary" htmlType="submit">Descargar Excel</Button>
+      </Form>
+      </div>
     )
   }
