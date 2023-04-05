8d8290e887bea7a226e123643a00a672f3583165
Erika Ladner <erikaladner5@gmail.com>
2023-04-05 15:14:10 -0300

    menu

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,7 +2,7 @@ import { FileOutlined, PieChartOutlined, UserOutlined, TeamOutlined ,DesktopOutl
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import { useState } from 'react';
 import Descargar from  './descargar'
-import logo from './descarga.png'
+import logo from './logo.png'
 import './layout.css'
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -15,15 +15,15 @@ function getItem(label, key, icon, children) {
   };
 }
 const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
+/*   getItem('Option 1', '1', <PieChartOutlined />),
   getItem('Option 2', '2', <DesktopOutlined />),
   getItem('User', 'sub1', <UserOutlined />, [
     getItem('Tom', '3'),
     getItem('Bill', '4'),
     getItem('Alex', '5'),
   ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]), */
+  getItem('Datos', '9', <FileOutlined />),
 ];
 const Layouts = () => {
   const [collapsed, setCollapsed] = useState(false);
@@ -45,14 +45,23 @@ const Layouts = () => {
             height: 28,
             margin: 16,
             background: 'rgba(255, 255, 255, 0.2)',
-          }}
-          className='imagen?'
+          }}  
         />
         <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
       </Sider>
       <Layout className="site-layout">
         <Header />
-        <Content>
+        <Content    style={{
+            margin: '0 16px',
+          }}>
+        <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Cargar Datos</Breadcrumb.Item>
+            
+          </Breadcrumb>
           <div>
           <Descargar/>
           </div>
